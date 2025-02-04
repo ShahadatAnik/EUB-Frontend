0e79be54cc10130f0b0f0ed531ef919a346efd93
Shafayat <shafayat1777@gmail.com>
2025-02-04 16:44:37 +0600

    ci: :sparkles: Added new component for faculty and staff for departments

--- src/app/courses/school-of-engineering/bsc-mechanical/_components/core-faculty-members.tsx
+++ src/app/courses/school-of-engineering/bsc-mechanical/_components/core-faculty-members.tsx
@@ -1,77 +1,9 @@
-import React from 'react';
-import data from '../_const/faculty-members-data';
-import Image from 'next/image';
-import { Mail, Phone } from 'lucide-react';
-import ContentWrapper from './content-wrapper';
+import React from "react";
+
+import FacultyAndStaff from "@/app/courses/_components/faculty-and-staff";
 
 const CoreFacultyMembers = () => {
-  return (
-    <ContentWrapper title='Core Faculty Members & Staff ' className='space-y-4'>
-      <div className='flex border rounded-md overflow-hidden'>
-        <div className=''>
-          <Image
-            src={'/person-placeholder.jpg'}
-            alt={'Placeholder'}
-            height={160}
-            width={160}
-          />
-        </div>
-
-        <div className='px-6 py-3 flex flex-col gap-0'>
-          <div>
-            <h3 className='text-xl font-semibold'>{data[0].name}</h3>
-            <p className='mt-1 text-muted-foreground'>{data[0].designation}</p>
-            <p className='text-muted-foreground'>{data[0].department}</p>
-          </div>
-          <div className='mt-2 text-muted-foreground'>
-            <p className='flex items-center gap-2'>
-              <Mail className='size-4' />
-              {data[0].email}
-            </p>
-            <p className='flex items-center gap-2'>
-              <Phone className='size-4' />
-              {data[0].mobile}
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className='grid grid-cols-2 gap-4 '>
-        {data.map((faculty, index) => (
-          <div key={index} className='flex  border rounded-md overflow-hidden'>
-            <div className=''>
-              <Image
-                src={faculty.image}
-                alt={faculty.name}
-                height={160}
-                width={160}
-              />
-            </div>
-
-            <div className='px-6 py-3 flex flex-col gap-0'>
-              <div>
-                <h3 className='text-xl font-semibold'>{faculty.name}</h3>
-                <p className='mt-1 text-muted-foreground'>
-                  {faculty.designation}
-                </p>
-                <p className='text-muted-foreground'>{faculty.department}</p>
-              </div>
-              <div className='mt-2 text-muted-foreground'>
-                <p className='flex items-center gap-2'>
-                  <Mail className='size-4' />
-                  {faculty.email}
-                </p>
-                <p className='flex items-center gap-2'>
-                  <Phone className='size-4' />
-                  {faculty.mobile}
-                </p>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </ContentWrapper>
-  );
+  return <FacultyAndStaff department="BSC-MECHANICAL" />;
 };
 
 export default CoreFacultyMembers;
