3ea1babe0ef4c09c310ca5b42ddf063b01c08862
alifbinhossain <alifahmed47@gmail.com>
2025-02-04 14:02:17 +0600

    docs: :wastebasket: removed evening program from business and arts

--- src/app/courses/school-of-arts-and-social-sciences/ba-english/_components/regular-program/program-details/index.tsx
+++ src/app/courses/school-of-arts-and-social-sciences/ba-english/_components/regular-program/program-details/index.tsx
@@ -6,16 +6,16 @@ const ProgramDetails = () => {
   return (
     <ContentWrapper title='Program Details'>
       <p>
-        The <strong>BA (Honours) in English</strong> at the European University
-        of Bangladesh is a broad-based, multidisciplinary program designed to
-        equip graduates with the skills needed to meet the demands of a rapidly
-        evolving society. The curriculum is structured around foundational
-        courses that broaden students’ perspectives, along with support courses
-        that enhance their understanding of core subjects. This four-year,
-        full-time day program consists of 12 regular terms, requiring students
-        to complete 40 courses totaling 120 credit hours. Students will receive
-        letter grades for their coursework, while the Comprehensive Examination
-        and thesis work will be graded on a pass/fail basis.
+        The BA (Honours) in English at the European University of Bangladesh is
+        a broad-based, multidisciplinary program designed to equip graduates
+        with the skills needed to meet the demands of a rapidly evolving
+        society. The curriculum is structured around foundational courses that
+        broaden students’ perspectives, along with support courses that enhance
+        their understanding of core subjects. This four-year, full-time day
+        program consists of 12 regular terms, requiring students to complete 40
+        courses totaling 120 credit hours. Students will receive letter grades
+        for their coursework, while the Comprehensive Examination and thesis
+        work will be graded on a pass/fail basis.
       </p>
     </ContentWrapper>
   );
