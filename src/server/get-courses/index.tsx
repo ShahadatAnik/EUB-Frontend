c6048b86644fe71afa445edb5ce18a0df73d09dc
Shafayat <shafayat1777@gmail.com>
2025-02-04 12:57:20 +0600

    ci: :bug: updated the fetch api for course schedule

--- src/server/get-courses/index.tsx
+++ src/server/get-courses/index.tsx
@@ -7,7 +7,7 @@ export const getRegularClassRoutine = async (
   department: string
 ): Promise<IDataTable[]> => {
   const res = await fetch(
-    `${secret.apiBaseUrl}/v1/portfolio/routine?department=${department}&program=regular&type=class`
+    `${secret.apiBaseUrl}/v1/portfolio/routine?portfolio_department=${department}&program=regular&type=class_routine `
   );
   return await res.json();
 };
@@ -15,7 +15,7 @@ export const getRegularExamSchedule = async (
   department: string
 ): Promise<IDataTable[]> => {
   const res = await fetch(
-    `${secret.apiBaseUrl}/v1/portfolio/routine?department=${department}&program=regular&type=exam`
+    `${secret.apiBaseUrl}/v1/portfolio/routine?portfolio_department=${department}&program=regular&type=exam_schedule`
   );
   return await res.json();
 };
@@ -24,7 +24,7 @@ export const getEveningClassRoutine = async (
   department: string
 ): Promise<IDataTable[]> => {
   const res = await fetch(
-    `${secret.apiBaseUrl}/v1/portfolio/routine?department=${department}&program=evening&type=class`
+    `${secret.apiBaseUrl}/v1/portfolio/routine?portfolio_department=${department}&program=evening&type=class_routine `
   );
   return await res.json();
 };
@@ -32,7 +32,7 @@ export const getEveningExamSchedule = async (
   department: string
 ): Promise<IDataTable[]> => {
   const res = await fetch(
-    `${secret.apiBaseUrl}/v1/portfolio/routine?department=${department}&program=evening&type=exam`
+    `${secret.apiBaseUrl}/v1/portfolio/routine?portfolio_department=${department}&program=evening&type=exam_schedule`
   );
   return await res.json();
 };
@@ -41,7 +41,7 @@ export const getDepartmentTeachers = async (
   department: string
 ): Promise<IDepartmentTeacher[]> => {
   const res = await fetch(
-    `${secret.apiBaseUrl}/v1/portfolio/department-teachers?department=${department}`
+    `${secret.apiBaseUrl}/v1/portfolio/department-teachers?portfolio_department=${department}`
   );
   return await res.json();
 };
