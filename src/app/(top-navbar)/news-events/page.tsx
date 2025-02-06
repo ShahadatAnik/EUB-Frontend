9614f5d365163da29f3b3c34cf49f97cc00d6d00
Shafayat <shafayat1777@gmail.com>
2025-02-06 19:17:38 +0600

    ci: :bug: removed a comment

--- src/app/(top-navbar)/news-events/page.tsx
+++ src/app/(top-navbar)/news-events/page.tsx
@@ -23,7 +23,7 @@ export default async function Page({ searchParams }: searchParamsProps) {
   console.log(data);
   return (
     <div>
-      {/* <PageHeader title="News & Events" image="/images/bg-2.jpg" />
+      <PageHeader title="News & Events" image="/images/bg-2.jpg" />
       <PageContainer>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {data.data.map((item: INewsPortal, index: number) => (
@@ -32,7 +32,7 @@ export default async function Page({ searchParams }: searchParamsProps) {
         </div>
 
         <PagePagination pagination={data.pagination} />
-      </PageContainer> */}
+      </PageContainer>
     </div>
   );
 }
