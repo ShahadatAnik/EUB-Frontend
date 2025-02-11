b4073e776d8a411f1059adb133027c787eb27065
Mirsad Al Hossain <51824780+m125712@users.noreply.github.com>
2025-02-11 16:50:30 +0600

    fix: :bug: remove version prefix from API endpoints in server functions

--- src/server/get-faculties.ts
+++ src/server/get-faculties.ts
@@ -5,7 +5,7 @@ import { IClubAndSociety } from "@/types";
 
 export const getFaculties = async (): Promise<IClubAndSociety[]> => {
 	const res = await fetch(
-		`${secret.apiBaseUrl}/v1/other/portfolio/faculty/value/label`
+		`${secret.apiBaseUrl}/other/portfolio/faculty/value/label`
 	);
 	return await res.json();
 };
--- src/server/getClub.ts
+++ src/server/getClub.ts
@@ -4,7 +4,7 @@ import { IClub } from "@/types";
 
 export const getClubs = async (faculty: string): Promise<IClub[]> => {
 	const res = await fetch(
-		`${secret.apiBaseUrl}/v1/portfolio/club?portfolio_faculty=${faculty}`
+		`${secret.apiBaseUrl}/portfolio/club?portfolio_faculty=${faculty}`
 	);
 	return await res.json();
 };
