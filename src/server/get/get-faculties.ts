0b8137ac84ae275b8da5c512a87323e3e2375556
Mirsad Al Hossain <51824780+m125712@users.noreply.github.com>
2025-02-12 14:15:53 +0600

    refactor: improve code consistency and formatting in clubs and societies page

--- src/server/get/get-faculties.ts
+++ src/server/get/get-faculties.ts
@@ -1,14 +1,14 @@
-'use server';
+"use server";
 
-import { secret } from '@/config/secret';
-import { IClubAndSociety } from '@/types';
+import { secret } from "@/config/secret";
+import { IClubAndSociety } from "@/types";
 
 export const getFaculties = async (): Promise<IClubAndSociety[]> => {
-  const res = await fetch(
-    `${secret.apiBaseUrl}/other/portfolio/faculty/value/label`,
-    {
-      cache: 'no-store',
-    }
-  );
-  return await res.json();
+	const res = await fetch(
+		`${secret.apiBaseUrl}/other/portfolio/faculty/value/label?page='clubs_and_society'`,
+		{
+			cache: "no-store",
+		}
+	);
+	return await res.json();
 };
