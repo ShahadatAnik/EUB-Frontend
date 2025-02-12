eacec42d6be1d6ab5321125a5fda15584cc3563c
Mirsad Al Hossain <51824780+m125712@users.noreply.github.com>
2025-02-12 13:13:18 +0600

    fix: :bug: update API endpoint to include page parameter for faculty retrieval

--- src/server/get-faculties.ts
+++ src/server/get-faculties.ts
@@ -5,7 +5,7 @@ import { IClubAndSociety } from "@/types";
 
 export const getFaculties = async (): Promise<IClubAndSociety[]> => {
 	const res = await fetch(
-		`${secret.apiBaseUrl}/other/portfolio/faculty/value/label`
+		`${secret.apiBaseUrl}/other/portfolio/faculty/value/label/?page=clubs_and_society`
 	);
 	return await res.json();
 };
