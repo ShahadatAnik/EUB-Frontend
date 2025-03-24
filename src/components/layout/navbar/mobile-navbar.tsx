fa3b4731d153de7a55613fc35a9a30f77d05999c
alifbinhossain <alifahmed47@gmail.com>
2025-03-24 14:31:03 +0600

    fix: :iphone: fixed mobile navbar issue

--- src/components/layout/navbar/mobile-navbar.tsx
+++ src/components/layout/navbar/mobile-navbar.tsx
@@ -2,7 +2,13 @@
 
 import React, { useState } from 'react';
 
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetTitle,
+  SheetDescription,
+} from '@/components/ui/sheet';
 
 import {
   Accordion,
@@ -29,61 +35,82 @@ const MobileNavbar = () => {
               <Menu />
             </Button>
           </SheetTrigger>
-          <SheetContent className='w-full space-y-8'>
+          <SheetContent className='w-full space-y-8 h-full overflow-auto'>
+            <SheetTitle className='sr-only'>
+              European University of Bangladesh
+            </SheetTitle>
+            <SheetDescription className='sr-only'>
+              The official website of the European University of Bangladesh
+            </SheetDescription>
             <BrandLogo />
             <Accordion type='single' collapsible className='w-full space-y-1'>
-              {navLinks.map((item, index) => (
-                <AccordionItem key={index} value={item.title}>
-                  <AccordionTrigger className='bg-primary px-4 py-3 text-white'>
-                    {item.title}
-                  </AccordionTrigger>
-                  <AccordionContent className='pt-1 pb-0 pl-2'>
-                    {!!item.children && item.children.length > 0 && (
-                      <ul className='space-y-3'>
-                        {item.children.map((child, index) => (
-                          <li key={index}>
-                            <div>
-                              {child.href ? (
-                                <Link
-                                  className='text-base font-bold text-primary hover:underline'
-                                  href={child.href!}
-                                >
-                                  {child.title}
-                                </Link>
-                              ) : (
-                                <h6 className='text-base font-bold text-primary'>
-                                  {child.title}
-                                </h6>
-                              )}
+              {navLinks.map((item, index) => {
+                if (item.children && item.children.length > 0) {
+                  return (
+                    <AccordionItem key={index} value={item.title}>
+                      <AccordionTrigger className='bg-primary px-4 py-3 text-white'>
+                        {item.title}
+                      </AccordionTrigger>
+                      <AccordionContent className='pt-1 pb-2 pl-2'>
+                        <ul className='space-y-3'>
+                          {item.children.map((child, index) => (
+                            <li key={index}>
+                              <div>
+                                {child.href ? (
+                                  <Link
+                                    onClick={() => setIsOpen(false)}
+                                    className='text-sm font-semibold text-primary hover:underline'
+                                    href={child.href!}
+                                  >
+                                    {child.title}
+                                  </Link>
+                                ) : (
+                                  <h6 className='text-sm font-semibold text-primary'>
+                                    {child.title}
+                                  </h6>
+                                )}
 
-                              {child.children && child.children.length > 0 && (
-                                <ul className='mt-1 space-y-1'>
-                                  {child.children.map((subChild, index) => (
-                                    <li key={index}>
-                                      {subChild.href ? (
-                                        <Link
-                                          onClick={() => setIsOpen(false)}
-                                          href={subChild.href!}
-                                          className='flex items-center gap-1 hover:underline'
-                                        >
-                                          <ChevronsRight className='size-4' />
-                                          {subChild.title}
-                                        </Link>
-                                      ) : (
-                                        <p>{subChild.title}</p>
-                                      )}
-                                    </li>
-                                  ))}
-                                </ul>
-                              )}
-                            </div>
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </AccordionContent>
-                </AccordionItem>
-              ))}
+                                {child.children &&
+                                  child.children.length > 0 && (
+                                    <ul className='mt-1 space-y-1'>
+                                      {child.children.map((subChild, index) => (
+                                        <li key={index}>
+                                          {subChild.href ? (
+                                            <Link
+                                              onClick={() => setIsOpen(false)}
+                                              href={subChild.href!}
+                                              className='flex items-center gap-1 hover:underline'
+                                            >
+                                              <ChevronsRight className='size-4' />
+                                              {subChild.title}
+                                            </Link>
+                                          ) : (
+                                            <p>{subChild.title}</p>
+                                          )}
+                                        </li>
+                                      ))}
+                                    </ul>
+                                  )}
+                              </div>
+                            </li>
+                          ))}
+                        </ul>
+                      </AccordionContent>
+                    </AccordionItem>
+                  );
+                }
+
+                return (
+                  <Link className='block' key={index} href={item.href!}>
+                    <Button
+                      onClick={() => setIsOpen(false)}
+                      className='w-full rounded-none justify-start'
+                    >
+                      {item.title}
+                    </Button>
+                  </Link>
+                );
+              })}
             </Accordion>
           </SheetContent>
         </Sheet>
