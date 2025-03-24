020b0cd03151637dbdeefe2e162b7ad28b8d3e05
alifbinhossain <alifahmed47@gmail.com>
2025-03-24 14:15:48 +0600

    refactor: :wastebasket: removed manually typed image url

--- src/components/client-image.tsx
+++ src/components/client-image.tsx
@@ -4,8 +4,7 @@ import React from 'react';
 import Image, { ImageProps } from 'next/image';
 
 export default function ClientImage({ src, alt, ...props }: ImageProps) {
-  const imageBaseUrl =
-    process.env.NEXT_PUBLIC_IMAGE_BASE_URL || 'http://103.147.163.46:4040';
+  const imageBaseUrl = process.env.NEXT_PUBLIC_IMAGE_BASE_URL;
 
   return (
     <Image
