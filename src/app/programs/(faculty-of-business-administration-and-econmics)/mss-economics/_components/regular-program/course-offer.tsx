5bf191c4e55e274419cb01bca7223a489245cd3e
Mirsad Al Hossain <51824780+m125712@users.noreply.github.com>
2025-05-17 14:35:06 +0600

    feat: course offer added in all department all programs

---
+++ src/app/programs/(faculty-of-business-administration-and-econmics)/mss-economics/_components/regular-program/course-offer.tsx
@@ -0,0 +1,19 @@
+import SystemTable from "@/components/table/system-table";
+import { columns, useGetRegularCourseOffer } from "@/hooks/use-get-course";
+import React from "react";
+import ContentWrapper from "../content-wrapper";
+
+const ClassRoutine = () => {
+	const { data } = useGetRegularCourseOffer("MSS-ECONOMICS");
+	return (
+		<ContentWrapper title="Course Offer">
+			<SystemTable
+				caption="Course Offer"
+				data={data!}
+				columns={columns}
+			/>
+		</ContentWrapper>
+	);
+};
+
+export default ClassRoutine;
