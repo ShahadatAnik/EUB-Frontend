a30fd7a98668deebb06cca45a8f345bc3af64ad9
alifbinhossain <alifahmed47@gmail.com>
2025-07-02 19:38:11 +0600

    feat: Add alumni images and update alumni industry page with new data structure

--- src/app/(programs)/(faculty-of-business-administration-and-econmics)/mgds-one-year/alumni-industry/page.tsx
+++ src/app/(programs)/(faculty-of-business-administration-and-econmics)/mgds-one-year/alumni-industry/page.tsx
@@ -1,17 +1,68 @@
 import React from 'react';
 
-import ContentWrapper from '@/app/(programs)/_components/content-wrapper';
-import UnderDevelopment from '@/app/(programs)/_components/under-development';
+import { IAlumni } from '@/types';
+
+import AlumniIndustry from '@/app/(programs)/_components/alumni-and-industry';
 
 // export const metadata = generateMetaData({
 // });
 
 const Page = () => {
-  return (
-    <ContentWrapper title='Alumni and Industry'>
-      <UnderDevelopment />
-    </ContentWrapper>
-  );
+  const data: IAlumni[] = [
+    {
+      name: 'Siam Sarower Jamil',
+      designation:
+        'PhD Research Scholar/Candidate, Department of Political Science, University of Delhi',
+      description: '',
+      image: '/person-placeholder.jpg',
+    },
+    {
+      name: 'Abdullah Al Mamun',
+      designation: 'Manager, Agrani Bank PLC',
+      description: '',
+      image: '/person-placeholder.jpg',
+    },
+    {
+      name: 'Md. Asaduzzaman',
+      designation: 'Inspector, Bangladesh Police',
+      description: '',
+      image: '/person-placeholder.jpg',
+    },
+    {
+      name: 'Mahmudoon Nabi',
+      designation: 'Sr.Deputy Manager, Energypac Engineering Ltd',
+      description: '',
+      image: '/person-placeholder.jpg',
+    },
+    {
+      name: 'Nue Mya Mong Marma',
+      designation:
+        'Cotton Development Officer, Increase Cotton Production & Poverty Alleviation of Farmer in Chittagong Hill Tracks, Chittagong Hill Tracts Development Board',
+      description: '',
+      image: '/person-placeholder.jpg',
+    },
+    {
+      name: 'S.M. MOKARRAM HOSSEN',
+      designation:
+        'Asst. Manager (Procurement), ANANDA SHIPYARD & SLIPWAYS LTD.',
+      description: '',
+      image: '/person-placeholder.jpg',
+    },
+    {
+      name: 'MD. Aminul Haque',
+      designation: 'Senior Project Officer, BRAC',
+      description: '',
+      image: '/person-placeholder.jpg',
+    },
+    {
+      name: 'MD Nizam Hosen',
+      designation:
+        'Camp Manager- Shelter & Site Development, NGO Forum for Public Health',
+      description: '',
+      image: '/person-placeholder.jpg',
+    },
+  ];
+  return <AlumniIndustry data={data} />;
 };
 
 export default Page;
